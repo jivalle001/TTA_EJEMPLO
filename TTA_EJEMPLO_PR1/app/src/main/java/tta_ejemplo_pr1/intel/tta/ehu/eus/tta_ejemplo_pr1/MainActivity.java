baeac6ad4647916204744f6e941e2b4514b305a9
Jose Ignacio Valle <jivalle001@ikasle.ehu.eus>
2016-12-20 18:37:21 +0100

    CREADA LA ESTRUCTURA INICIAL DE LA APP DE EJEMPLO

---
+++ TTA_EJEMPLO_PR1/app/src/main/java/tta_ejemplo_pr1/intel/tta/ehu/eus/tta_ejemplo_pr1/MainActivity.java
@@ -0,0 +1,13 @@
+package tta_ejemplo_pr1.intel.tta.ehu.eus.tta_ejemplo_pr1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
