d9a1cb7100eeffb44344b36c9b7580969635f438
Jose Ignacio Valle <jivalle001@ikasle.ehu.eus>
2017-01-13 18:42:35 +0100

    Hilo para la recoger usuario de servidor empezado y creada la clase User.

--- Ejemplo/app/src/main/java/eus/ehu/ejemplo/MainActivity.java
+++ Ejemplo/app/src/main/java/eus/ehu/ejemplo/MainActivity.java
@@ -1,15 +1,22 @@
 package eus.ehu.ejemplo;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
 
-    public final static String EXTRA_LOGIN = "tta_ejemplo_pr1.intel.tta.ehu.eus.tta_ejemplo_pr1.login";
+    public final static String EXTRA_LOGIN = "eus.ehu.ejemplo.login";
+    RestClient restClient = new RestClient("http://u017633.ehu.eus:28080/ServidorTta/rest/tta");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,4 +39,41 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
         }
    }
+
+    public void authentication(View view) throws IOException {
+        final String dni = ((EditText)findViewById(R.id.login)).getText().toString();
+        final String passwd = ((EditText)findViewById(R.id.passwd)).getText().toString();
+        new AsyncTask<Void,Void,Void>(){
+            User user;
+            @Override
+            protected Void doInBackground(Void... voids) {
+                try {
+                    restClient.setHttpBasicAuth(dni,passwd);
+                    JSONObject json = restClient.getJson(String.format("getStatus?id=%s",dni));
+                    user = new User(json.getInt("id"),json.getString("user"),json.getInt("lessonNumber"),json.getString("lessonTitle"),json.getInt("nextText"),json.getInt("nextExercise"));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                super.onPostExecute(aVoid);
+                Intent intent = new Intent(getApplicationContext(),MenuActivity.class);
+                if(dni.matches("") || passwd.matches("")) {
+                    Toast.makeText(getApplicationContext(),"¡Login Incorrecto!",Toast.LENGTH_LONG).show();
+                }
+                else {
+                    intent.putExtra(MainActivity.EXTRA_LOGIN,dni);
+                    Toast.makeText(getApplicationContext(),"¡Login Correcto!",Toast.LENGTH_SHORT).show();
+                    startActivity(intent);
+                }
+            }
+        }.execute();
+    }
+
+
 }
---
+++ Ejemplo/app/src/main/java/eus/ehu/ejemplo/User.java
@@ -0,0 +1,75 @@
+package eus.ehu.ejemplo;
+
+/**
+ * Created by jose on 13/01/17.
+ */
+public class User {
+
+    private int id;
+    private String user;
+    private int lessonNumber;
+    private String lessonTitle;
+    private int nextTest;
+    private int nextExercise;
+
+    public User() {
+
+    }
+
+    public User(int id, String user, int lessonNumber, String lessonTitle, int nextTest, int nextExercise) {
+        this.id=id;
+        this.user=user;
+        this.lessonNumber=lessonNumber;
+        this.lessonTitle=lessonTitle;
+        this.nextTest=nextTest;
+        this.nextExercise=nextExercise;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public int getLessonNumber() {
+        return lessonNumber;
+    }
+
+    public void setLessonNumber(int lessonNumber) {
+        this.lessonNumber = lessonNumber;
+    }
+
+    public String getLessonTitle() {
+        return lessonTitle;
+    }
+
+    public void setLessonTitle(String lessonTitle) {
+        this.lessonTitle = lessonTitle;
+    }
+
+    public int getNextTest() {
+        return nextTest;
+    }
+
+    public void setNextTest(int nextTest) {
+        this.nextTest = nextTest;
+    }
+
+    public int getNextExercise() {
+        return nextExercise;
+    }
+
+    public void setNextExercise(int nextExercise) {
+        this.nextExercise = nextExercise;
+    }
+}
